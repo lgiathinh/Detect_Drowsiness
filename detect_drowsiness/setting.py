e6cbc8beff84f0bc7ed475b395cb09cbc588ebe3
lgiathinh <lieuthinh97@gmail.com>
2025-06-29 12:16:19 +0700

    Add files via upload

---
+++ detect_drowsiness/setting.py
@@ -0,0 +1,11 @@
+def init():
+    global lat_deg, lat_min, lat_dir, long_deg, long_min, long_dir, new_location, M, speed
+    lat_deg= 0
+    lat_min= 0.0
+    lat_dir= str(0)
+    long_deg= 0
+    long_min= 0.0
+    long_dir= str(0)
+    speed = 0
+    new_location = 0
+    M = {}
